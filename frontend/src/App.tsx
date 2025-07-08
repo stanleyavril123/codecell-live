518b3b2b7b6aece25b5664fa07afb92be844f639
Stanley Avril <stanley.avril@outlook.com>
2025-07-08 19:09:51 -0400

    monaco code editor imported

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { Box } from "@mui/material";
 import ThemeToggle from "./components/ThemeToggle";
+import CodeEditor from "./components/CodeEditor";
 
 function App() {
   return (
     <Box>
       <ThemeToggle />
+      <CodeEditor/>
     </Box>
   );
 }
---
+++ frontend/src/components/CodeEditor.tsx
@@ -0,0 +1,13 @@
+import Editor from "@monaco-editor/react";
+
+const CodeEditor = () => {
+  return (
+    <Editor
+      height="90vh"
+      defaultLanguage="javascript"
+      defaultValue="// some comment"
+    />
+  );
+};
+
+export default CodeEditor;
