84903fa24abfc4afa62edac646e9b4cf3ee7b04e
Stanley Avril <stanley.avril@outlook.com>
2025-07-11 16:56:54 -0400

    focus on editior when page load in

--- frontend/src/components/CodeEditor.tsx
+++ frontend/src/components/CodeEditor.tsx
@@ -1,11 +1,23 @@
+import type * as Monaco from "monaco-editor";
 import Editor from "@monaco-editor/react";
+import { useRef } from "react";
+
+type EditorType = Monaco.editor.IStandaloneCodeEditor;
 
 const CodeEditor = () => {
+  const editorRef = useRef<EditorType | null>(null);
+
+  const onMount = (editor: EditorType) => {
+    editorRef.current = editor;
+    editor.focus();
+  };
   return (
     <Editor
       height="90vh"
+      value=""
+      theme="vs-dark"
+      onMount={onMount}
       defaultLanguage="javascript"
-      defaultValue="// some comment"
     />
   );
 };
