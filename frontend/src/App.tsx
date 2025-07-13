a0b1052b3139cf167157ca403b82c692c5e02735
Stanley Avril <stanley.avril@outlook.com>
2025-07-12 21:13:01 -0400

    lang selector display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,14 @@ import { Box } from "@mui/material";
 import ThemeToggle from "./components/ThemeToggle";
 import CodeEditor from "./components/CodeEditor";
 import RunButton from "./components/RunButton";
+import LanguageSelector from "./components/LanguageSelector";
 
 function App() {
   return (
     <Box>
       <ThemeToggle />
       <RunButton />
+      <LanguageSelector />
       <CodeEditor />
     </Box>
   );
---
+++ frontend/src/components/LanguageSelector.tsx
@@ -0,0 +1,52 @@
+import { Box, Button, Menu, MenuItem } from "@mui/material";
+import { useState } from "react";
+import { LANGUAGE_VERSION } from "../constants.ts";
+
+const languages = Object.entries(LANGUAGE_VERSION);
+
+const LanguageSelector = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        Language :
+      </Button>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          list: {
+            "aria-labelledby": "basic-button",
+          },
+        }}
+      >
+        {languages.map(([lang, version]) => (
+          <MenuItem key={lang} onClick={handleClose}>
+            {lang} - v{version}
+          </MenuItem>
+        ))}
+      </Menu>
+    </Box>
+  );
+};
+
+export default LanguageSelector;
